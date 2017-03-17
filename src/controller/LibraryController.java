f68ad33256a1f6e9477ff8474a245ae556c5774b
Daniel Garcia <asl021@my.utsa.edu>
2017-03-17 17:13:06 -0500

    Organized Imports

--- src/controller/LibraryController.java
+++ src/controller/LibraryController.java
@@ -1,6 +1,7 @@
 package controller;
 import java.util.HashSet;
 import java.util.Set;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
--- src/gateway/SongGateway.java
+++ src/gateway/SongGateway.java
@@ -1,8 +1,6 @@
 package gateway;
 
 import java.io.File;
-
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
@@ -12,13 +10,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import com.mysql.jdbc.Driver;
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 import helliker.id3.CorruptHeaderException;
 import helliker.id3.ID3v2FormatException;
